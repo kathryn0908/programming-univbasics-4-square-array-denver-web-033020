471f64d397bf892994625e2bc26787482267424f
Kathryn Clark <kathrynlael@gmail.com>
2020-03-06 18:07:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   count=1  
   numbers=[1,2,3]
   while numbers[count] do
-  numbers.each{numbers[count]**numbers[count]} 
+  numbers.each{count**2} 
   count+=1  
   end
   
