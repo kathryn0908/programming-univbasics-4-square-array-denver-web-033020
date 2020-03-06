0164dd2ac58abf4a33d8ae46a4084722cf69fe6d
Kathryn Clark <kathrynlael@gmail.com>
2020-03-06 18:12:22 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,8 @@
 def square_array(numbers)
   count=0 
-  numbers=[1,2,3]
   new_array=[]
-  while numbers[count] do
-  numbers.each{|i| new_array << i*i} 
+  while array[count] do
+  array.each{|i| new_array << i*i} 
   new_array
   count+=1  
   end
